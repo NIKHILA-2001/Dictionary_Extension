c2b3c706af711efdded0124c9519964acea816ac
NIKHILA-2001 <71930062+NIKHILA-2001@users.noreply.github.com>
2023-07-29 11:00:35 +0530

    Update Script.js
    Multiple meanings

--- Script.js
+++ Script.js
@@ -1,16 +1,24 @@
   let w;
   const d = [];
   let count = 0;
-  let def, res;
+  let def="", res;
   document.getElementById("myButton").addEventListener("click", getWord);
 async function getWord() {
     w=document.querySelector(".inp").value;
     const words = await fetch(
       "https://api.dictionaryapi.dev/api/v2/entries/en_US/"+w);
     const d = await words.json();
+    console.log(d[0].meanings.length);
     if (d.length == 1 || d.length == 2) count = count + 1;
+    let meanings = d[0].meanings;
+    let c=1;
+    
+    let str="";
+    meanings.forEach(e => {
+      str +=  c.toString()+". "+e.definitions[0].definition+"\n";
+      console.log(str);
+      c+=1;
+    });
     res = d[0].word;
-    def = d[0].meanings[0].definitions[0].definition;
-    document.querySelector(".out").innerText=def;
-    console.log(def);
+    document.querySelector(".out").innerText=str;
 }
